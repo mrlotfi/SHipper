38e5208ec3034a8409fa6f59fbd0b16e7f05dcfc
pexpert28 <Apple@Pooya-Mahmoodi.local>
2014-12-24 21:53:59 +0330

    problem

--- EclipseProject/src/console/ConsoleRunner.java
+++ EclipseProject/src/console/ConsoleRunner.java
@@ -1,7 +1,25 @@
 package console;
 
+import java.util.*;
+import game.Player;
+
 public class ConsoleRunner {
 	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
 		
+		for (int i = 0; i < 2; i++) {
+			int m = input.nextInt();//  tool e safhe 
+			int n = input.nextInt();//  arz e safhe
+			input.nextLine();
+			
+			if(i==0)
+				Player Player1 = new Player(m,n,0); 
+			
+			if(i==1)
+				Player Player2 = new Player(m,n,1);
+		}
+		
+		
+
 	}
 }
